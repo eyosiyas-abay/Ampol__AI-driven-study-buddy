8bc85cd6ede6b7c0b33e160282fa58aec0069e3e
eyosiyas-abay <eyosiyasabay.contact@gmail.com>
2025-05-04 14:48:03 +0300

    update: register validation, save, error handling

--- app/Livewire/Auth/RegisterForm.php
+++ app/Livewire/Auth/RegisterForm.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\EmailVerification;
+use App\Models\User;
 use Livewire\Component;
 
 class RegisterForm extends Component
@@ -9,20 +11,36 @@ class RegisterForm extends Component
     public string $name = '';
     public string $email = '';
     public string $password = '';
+    public string $passwordConfirm = '';
+
+    protected $rules =  [
+        'name' => 'required|string|max:255',
+        'email' => 'required|email|max:255|unique:users,email',
+        'password' => 'required|string|min:8',
+        'passwordConfirm' => 'required|string|min:8|same:password',
+    ];
+
+    protected $messages = [
+        'passwordConfirm.same' => 'The password confirmation does not match.',
+    ];
+
+    public function registerUser(): void
+    {
+        $user = tap(new User, fn($user) =>  $user->fill([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => $this->password
+        ]));
+
+        $user->sendEmailVerificationNotification();
+    }
 
     public function submit()
     {
         $this->validate();
+        $this->registerUser();
     }
 
-    public function rules(): array
-    {
-        return [
-            'name' => 'required|string|max:255',
-            'email' => 'required|email|max:255|unique:users,email',
-            'password' => 'required|string|min:8|confirmed',
-        ];
-    }
 
     public function render()
     {
